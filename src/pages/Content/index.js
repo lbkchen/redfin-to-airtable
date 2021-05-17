c9bbf6ee285f6313f12b43c39cd9f34532c0e87c
Ken Chen <ken.chen@samsara.com>
2021-05-16 22:03:01 -0700

    Basic message sending

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -3,4 +3,13 @@ import { printLine } from './modules/print';
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log(
+    sender.tab
+      ? 'from a content script:' + sender.tab.url
+      : 'from the extension'
+  );
+  if (request.greeting == 'hello') sendResponse({ farewell: 'goodbye' });
+});
+
 printLine("Using the 'printLine' function from the Print Module");
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -3,14 +3,26 @@ import logo from '../../assets/img/logo.svg';
 import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
 
+
 const Popup = () => {
+
+  const test = () => {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+        console.log(response.farewell);
+      });
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/pages/Popup/Popup.js</code> and save to reload.
+
         </p>
+        <button onClick={test}>TEST</button>
         <a
           className="App-link"
           href="https://reactjs.org"
